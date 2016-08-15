663483b547098c5fb41b864e00517275eb13d0e4
alina-portna <a.portna@gmail.com>
2016-08-15 19:23:52 +0300

    комит делается для проверки зеркала

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
 # made for integration testing
 # I added one more string
+# I'd like to check if this change will be commited to the Alina-Int on git.intent after commiting it to my own AlinaInt
+#Alina-Int is mirror, 1 hour was set in the settings
